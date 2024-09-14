a5b085af4f7b6e0836e58b2f1e128b8d9381ab70
Adrian San Segundo <adrisanse.ss@gmail.com>
2024-09-14 14:11:35 +0200

    feat: add bio text transition

--- src/app/profile-card/profile-card.component.ts
+++ src/app/profile-card/profile-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-card',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss'
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
+  displayedTextArray: string[] = [];
+  bioText = 'Adri is a software engineer with over 3 years of experience in developing web and mobile applications. He is skilled in Javascript, Typescript, Angular and Node.js.';
+  indexToShow = 0;
+
+  ngOnInit(): void {
+    this.displayedTextArray = this.bioText.split(' ');
+    this.typeWriter();
+  }
+
+  typeWriter() {
+    if (this.indexToShow < this.displayedTextArray.length) {
+      this.indexToShow++;
+      setTimeout(() => this.typeWriter(), 50);
+    }
+  }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        typing: {
+          '0%': { width: '0%' },
+          '100%': { width: '100%' },
+        },
+        blink: {
+          '50%': { borderColor: 'transparent' },
+          '100%': { borderColor: 'black' },
+        },
+      },
+      animation: {
+        typing: 'typing 3s steps(30, end) forwards',
+        blink: 'blink 1s step-end infinite',
+      },
+    },
   },
   plugins: [],
 };
